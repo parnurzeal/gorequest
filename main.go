f71ab3d97b8ec10a81d500c8b0a8a70f13dfd728
Theeraphol Wattanavekin <parnurzeal@gmail.com>
2014-02-20 17:46:01 +0900

    CustomRequest is able to work for POST-json

--- main.go
+++ main.go
@@ -35,13 +35,13 @@ func CustomRequest(options Options) (error, *http.Response, string){
     resp *http.Response
   )
   client := &http.Client{}
-  if options.json != ""{
-    content := strings.NewReader(options.json)
-    req, err = http.NewRequest(options.method, options.url, content)
-    req.Header.Add("Content-Type","application/json")
-    fmt.Println("inside", req)
+  if options.method == "POST" {
+    if options.json != ""{
+      content := strings.NewReader(options.json)
+      req, err = http.NewRequest(options.method, options.url, content)
+      req.Header.Set("Content-Type","application/json")
+    }
   }
-  fmt.Println("outside", req)
   resp, err = client.Do(req)
   if err!=nil{
     return err, nil, ""
@@ -52,16 +52,26 @@ func CustomRequest(options Options) (error, *http.Response, string){
 }
 
 func main(){
-  err, response, body:= Get("http://localhost:1337")
+  /*err, response, body:= Get("http://localhost:1337")
   if err==nil && response.StatusCode == 200 {
     fmt.Println(body)
   }
-  fmt.Println(err, response, body)
-  options:= Options{ url: "http://localhost:1337", method:"GET", body:"hello", json:`{ "hello":"hello"}`}
+  fmt.Println(err, response, body)*/
+  options:= Options{ url: "http://localhost:1337", method:"POST", body:"hello", json:`{ "hello":"hello"}`}
   CustomRequest(options)
 
-  const jsonStream =`{"sn":"sn1"}`
-  reader:=strings.NewReader(jsonStream)
-  http.Post("http://localhost:1337", "application/json", reader)
 
+  /*client:= &http.Client{}
+  req,_ := http.NewRequest("GET", "http://localhost:1337", nil)
+  req.Header.Add("Content-Type","application/json")
+  fmt.Println("main",req)
+  res, _ :=  client.Do(req)
+  fmt.Println(res.Body)
+  /*const jsonStream =`{"sn":"sn1"}`
+  reader:=strings.NewReader(jsonStream)
+  resp,_ := http.Post("http://localhost:1337", "application/json", reader)
+  defer resp.Body.Close()
+  body,_ :=ioutil.ReadAll(resp.Body)
+  fmt.Println(resp)
+  fmt.Println(string(body))*/
 }
