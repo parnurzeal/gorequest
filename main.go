ffcc964b498a91df939aeb445bb95a1b78b06278
theeraphol <theeraphol.wat@gmail.com>
2014-05-02 17:06:45 +0900

    Added document to End()

--- main.go
+++ main.go
@@ -257,6 +257,11 @@ func changeMapToURLValues(data map[string]interface{}) url.Values {
 	return newUrlValues
 }
 
+// End is the most important function that you need to call when ending the chain. The request won't proceed without calling it.
+// End function returns Response which matchs the structure of Response type in Golang's http package (but without Body data). The body data itself returns as a string in a 2nd return value.
+// Lastly but worht noticing, error array (NOTE: not just single error value) is returned as a 3rd value and nil otherwise.
+//
+//
 func (s *SuperAgent) End(callback ...func(response Response, body string)) (Response, string, []error) {
 	var (
 		req  *http.Request
