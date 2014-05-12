7c2ea848d381fc7f73a229dd8256bc223c31ab4c
theeraphol <theeraphol.wat@gmail.com>
2014-05-12 17:49:20 +0900

    Added doc for Proxy func

--- main.go
+++ main.go
@@ -176,6 +176,16 @@ func (s *SuperAgent) Query(content string) *SuperAgent {
 	return s
 }
 
+// Proxy function accepts a proxy url string to setup proxy url for any request.
+// It provides a convenience way to setup proxy which have advantages over usual old ways.
+// One example is you might try to set `http_proxy` environment. This means you are setting proxy up for all the requests.
+// You will not be able to send different request with different proxy unless you change your `http_proxy` environment again.
+// Another example is using Golang proxy setting. This is normal prefer way to do but too verbase compared to GoRequest's Proxy:
+//
+//      gorequest.New().Proxy("http://myproxy:9999").
+//        Post("http://www.google.com").
+//        End()
+//
 func (s *SuperAgent) Proxy(proxyUrl string) *SuperAgent {
 	parsedProxyUrl, err := url.Parse(proxyUrl)
 	if err != nil {
