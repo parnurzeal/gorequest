c05b171f4d1371449f3b27648f771081ff0d6a22
ParNurZeal <parnurzeal@gmail.com>
2014-07-27 19:03:46 +0900

    Added benchmark for json string Send

---
+++ benchmark_test.go
@@ -0,0 +1,21 @@
+package gorequest
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func BenchmarkPostFromSendJson(b *testing.B) {
+	request := New()
+	for n := 0; n < b.N; n++ {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		}))
+		request.Post(ts.URL).
+			Send(`{"query1":"test"}`).
+			Send(`{"query2":"test"}`).
+			End()
+		ts.Close()
+	}
+
+}
