05bd38166222c4071cf65de85aa84346f1bb1de7
Chris Marshall <chris@chrismar035.com>
2016-10-08 09:21:38 -0400

    Document RedirectPolicy

--- gorequest.go
+++ gorequest.go
@@ -479,6 +479,12 @@ func (s *SuperAgent) Proxy(proxyUrl string) *SuperAgent {
 	return s
 }
 
+// RedirectPolicy accepts a function to define how to handle redirects. If the
+// policy function returns an error, the next Request is not made and the previous
+// request is returned.
+//
+// The policy function's arguments are the Request about to be made and the
+// past requests in order of oldest first.
 func (s *SuperAgent) RedirectPolicy(policy func(req Request, via []Request) error) *SuperAgent {
 	s.Client.CheckRedirect = func(r *http.Request, v []*http.Request) error {
 		vv := make([]Request, len(v))
