f3c7f958bbc7d64abadf56b0d28b97e2aaf90435
huangyu <yu.huang@socialcredits.cn>
2018-05-04 15:29:51 +0800

    rewrite changeMapToURLValues function

--- gorequest.go
+++ gorequest.go
@@ -871,72 +871,41 @@ func (s *SuperAgent) SendFile(file interface{}, args ...string) *SuperAgent {
 	return s
 }
 
+func changeMapToURLValuesLoop(urlValues *url.Values, k string, data interface{}) {
+	dataVal := reflect.ValueOf(data)
+	dataKind := getKind(dataVal)
+	switch dataKind {
+	case reflect.Bool:
+		urlValues.Add(k, strconv.FormatBool(dataVal.Bool()))
+	case reflect.String:
+		urlValues.Add(k, dataVal.String())
+	case reflect.Int:
+		urlValues.Add(k, strconv.FormatInt(dataVal.Int(), 10))
+	case reflect.Uint:
+		urlValues.Add(k, strconv.FormatUint(dataVal.Uint(), 10))
+	case reflect.Float32:
+		urlValues.Add(k, strconv.FormatFloat(dataVal.Float(), 'f', -1, 64))
+	case reflect.Ptr:
+		changeMapToURLValuesLoop(urlValues, k, dataVal.Elem().Interface())
+	case reflect.Slice:
+		for i := 0; i < dataVal.Len(); i++ {
+			changeMapToURLValuesLoop(urlValues, k, dataVal.Index(i).Interface())
+		}
+	case reflect.Map: // exist this case? to check & test ??? TODO
+		for _, mk := range dataVal.MapKeys() {
+			changeMapToURLValuesLoop(urlValues, k, dataVal.MapIndex(mk).Interface())
+			//changeMapToURLValuesLoop(urlValues, mk.String(), dataVal.MapIndex(mk).Interface())
+		}
+	case reflect.Struct: // exist this case? to check & test ??? TODO consider how to use fieldName tag etc.
+	case reflect.Invalid: // TODO
+	default:  // TODO
+	}
+}
+
 func changeMapToURLValues(data map[string]interface{}) url.Values {
 	var newUrlValues = url.Values{}
 	for k, v := range data {
-		switch val := v.(type) {
-		case string:
-			newUrlValues.Add(k, val)
-		case bool:
-			newUrlValues.Add(k, strconv.FormatBool(val))
-		// if a number, change to string
-		// json.Number used to protect against a wrong (for GoRequest) default conversion
-		// which always converts number to float64.
-		// This type is caused by using Decoder.UseNumber()
-		case json.Number:
-			newUrlValues.Add(k, string(val))
-		case int:
-			newUrlValues.Add(k, strconv.FormatInt(int64(val), 10))
-		// TODO add all other int-Types (int8, int16, ...)
-		case float64:
-			newUrlValues.Add(k, strconv.FormatFloat(float64(val), 'f', -1, 64))
-		case float32:
-			newUrlValues.Add(k, strconv.FormatFloat(float64(val), 'f', -1, 64))
-		// following slices are mostly needed for tests
-		case []string:
-			for _, element := range val {
-				newUrlValues.Add(k, element)
-			}
-		case []int:
-			for _, element := range val {
-				newUrlValues.Add(k, strconv.FormatInt(int64(element), 10))
-			}
-		case []bool:
-			for _, element := range val {
-				newUrlValues.Add(k, strconv.FormatBool(element))
-			}
-		case []float64:
-			for _, element := range val {
-				newUrlValues.Add(k, strconv.FormatFloat(float64(element), 'f', -1, 64))
-			}
-		case []float32:
-			for _, element := range val {
-				newUrlValues.Add(k, strconv.FormatFloat(float64(element), 'f', -1, 64))
-			}
-		// these slices are used in practice like sending a struct
-		case []interface{}:
-
-			if len(val) <= 0 {
-				continue
-			}
-
-			switch val[0].(type) {
-			case string:
-				for _, element := range val {
-					newUrlValues.Add(k, element.(string))
-				}
-			case bool:
-				for _, element := range val {
-					newUrlValues.Add(k, strconv.FormatBool(element.(bool)))
-				}
-			case json.Number:
-				for _, element := range val {
-					newUrlValues.Add(k, string(element.(json.Number)))
-				}
-			}
-		default:
-			// TODO add ptr, arrays, ...
-		}
+		changeMapToURLValuesLoop(&newUrlValues, k, v)
 	}
 	return newUrlValues
 }
