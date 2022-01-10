a04151c12588897a330ebec8bfbb2369f1a4f0bd
wklken <wklken@gmail.com>
2022-01-10 14:54:33 +0800

    feat(setheaders): support gorequest.New().SetHeaders(map|struct)
    from https://github.com/parnurzeal/gorequest/pull/231 and do some changes, thanks to @razine-bensari

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/elazarl/goproxy v0.0.0-20211114080932-d06c3be7c11b
 	github.com/smartystreets/goconvey v1.7.2 // indirect
+	github.com/spf13/cast v1.4.1
 	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f
 	moul.io/http2curl v1.0.0
 )
--- gorequest.go
+++ gorequest.go
@@ -22,6 +22,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/spf13/cast"
 	"golang.org/x/net/publicsuffix"
 	"moul.io/http2curl"
 )
@@ -285,6 +286,58 @@ func (s *SuperAgent) Options(targetUrl string) *SuperAgent {
 //      End()
 func (s *SuperAgent) Set(param string, value string) *SuperAgent {
 	s.Header.Set(param, value)
+	return s
+}
+
+// SetHeaders is used for setting all your headers with the use of a map or a struct.
+// It uses AppendHeader() method so it allows for multiple values of the same header
+// Example. To set the following struct as headers, simply do
+//
+//    headers := apiHeaders{Accept: "application/json", Content-Type: "text/html", X-Frame-Options: "deny"}
+//    gorequest.New().
+//      Post("apiEndPoint").
+//      Set(headers).
+//      End()
+func (s *SuperAgent) SetHeaders(headers interface{}) *SuperAgent {
+	switch v := reflect.ValueOf(headers); v.Kind() {
+	case reflect.Struct:
+		s.setHeadersStruct(v.Interface())
+	case reflect.Map:
+		s.setHeadersMap(v.Interface())
+	default:
+		return s
+	}
+	return s
+}
+
+func (s *SuperAgent) setHeadersMap(content interface{}) *SuperAgent {
+	return s.setHeadersStruct(content)
+}
+
+// SendStruct (similar to SendString) returns SuperAgent's itself for any next chain and takes content interface{} as a parameter.
+// Its duty is to transform interface{} (implicitly always a struct) into s.Data (map[string]interface{}) which later changes into appropriate format such as json, form, text, etc. in the End() func.
+func (s *SuperAgent) setHeadersStruct(content interface{}) *SuperAgent {
+	if marshalContent, err := json.Marshal(content); err != nil {
+		s.Errors = append(s.Errors, err)
+	} else {
+		var val map[string]interface{}
+		d := json.NewDecoder(bytes.NewBuffer(marshalContent))
+		d.UseNumber()
+		if err := d.Decode(&val); err != nil {
+			s.Errors = append(s.Errors, err)
+		} else {
+			for k, v := range val {
+				strValue, err := cast.ToStringE(v)
+				if err != nil {
+					// TODO: log err?
+					continue
+				}
+
+				s.AppendHeader(k, strValue)
+			}
+
+		}
+	}
 	return s
 }
 
