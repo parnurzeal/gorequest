3784d51f2ecac7c2bf4ed8afb63365793346ac9f
wklken <wklken@gmail.com>
2022-02-12 01:31:52 +0800

    feat(timeout): support http client timeout details

--- gorequest.go
+++ gorequest.go
@@ -11,6 +11,7 @@ import (
 	"io/ioutil"
 	"log"
 	"mime/multipart"
+	"net"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httputil"
@@ -1438,6 +1439,39 @@ func (s *SuperAgent) safeModifyHttpClient() {
 func (s *SuperAgent) Timeout(timeout time.Duration) *SuperAgent {
 	s.safeModifyHttpClient()
 	s.Client.Timeout = timeout
+	return s
+}
+
+type Timeouts struct {
+	Dial      time.Duration
+	KeepAlive time.Duration
+
+	TlsHandshake   time.Duration
+	ResponseHeader time.Duration
+	ExpectContinue time.Duration
+	IdleConn       time.Duration
+}
+
+func (s *SuperAgent) Timeouts(timeouts *Timeouts) *SuperAgent {
+	s.safeModifyHttpClient()
+
+	transport, ok := s.Client.Transport.(*http.Transport)
+	if !ok {
+		return s
+	}
+
+	transport.DialContext = (&net.Dialer{
+		Timeout:   timeouts.Dial,
+		KeepAlive: timeouts.KeepAlive,
+	}).DialContext
+
+	transport.TLSHandshakeTimeout = timeouts.TlsHandshake
+	transport.ResponseHeaderTimeout = timeouts.ResponseHeader
+	transport.ExpectContinueTimeout = timeouts.ExpectContinue
+	transport.ExpectContinueTimeout = timeouts.IdleConn
+
+	s.Client.Transport = transport
+
 	return s
 }
 
